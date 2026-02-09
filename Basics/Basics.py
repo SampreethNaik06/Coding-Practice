d27d4fc176001ebc7834ad0e384ac06a628fbe12
Sampreethnaik06 <sampreethnaik032gmail.com>
2026-02-09 11:18:59 +0530

    basic files

---
+++ Basics/Basics.py
@@ -0,0 +1,7 @@
+first_name = "Sampreeth"
+last_name = "Naik"
+print(f"{first_name} {last_name}")
+
+## these all are basics today i want to warmup some of my basics befor i go to advance topics in python
+
+
