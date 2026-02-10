0a157756b1674a3d28fbf91a51af48da80160a39
Sampreethnaik06 <sampreethnaik032gmail.com>
2026-02-10 09:50:01 +0530

    still in some basics

--- Basics/Basics.py
+++ Basics/Basics.py
@@ -5,3 +5,31 @@ print(f"{first_name} {last_name}")
 ## these all are basics today i want to warmup some of my basics befor i go to advance topics in python
 
 
+age  = 10 
+name =  "Sampreeth"
+gender = "Male"
+print(type(age)) # int
+print(type(name)) # str
+
+
+
+## to check if the given number is positive or negative
+
+number = int(input("Enter a number: "))
+if number >= 0:
+    print("The number is positive.")
+else:    print("The number is negative.")
+
+
+str = "sampreeth naik"
+
+for i in str:
+    print(i)
+    
+### while loop executes until its true
+
+num = 10
+while num<10:
+    print(num)
+    num+=1
+    
