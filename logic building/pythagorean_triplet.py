b892df4a311b4d139fd989d5dd6e6030918717b8
SampreethNaik06 <sampreethnaik03@gmail.com>
2026-02-13 19:07:17 +0530

    started with level 4

---
+++ logic building/pythagorean_triplet.py
@@ -0,0 +1,12 @@
+# to check if the three numbers form a pythagorean triplet
+
+def check_triplet(a,b,c):
+    if a**2 +b**2 == c**2:
+        print("it is a pythagorean triplet")
+    else:
+        print("it is not a pythagorean triplet ")
+        
+check_triplet(3,4,5)
+
+
+# if the three values is divisible by 2 it is not primitive
