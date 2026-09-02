17385a76719b4a8dff07b2200e3c237bd1e1d1e9
SampreethNaik06 <sampreethnaik03@gmail.com>
2026-09-02 14:01:48 -0300

    wake up bro you are still week

--- Strivers Sheet/all_divisors.py
+++ Strivers Sheet/all_divisors.py
@@ -1,10 +1,18 @@
 # all divisors
+import math
 
-def divisor(n): 
-    for i in range(1,n+1):
-        if n % i ==0 :
-            print(i)
-
-    return 
+def divisor(n):
+    divisors = []
+    # Loop from 1 to floor(sqrt(n))
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            # Avoid adding the square root twice if n is a perfect square
+            if n // i != i:
+                divisors.append(n // i)
+    
+    divisors.sort()  # Optional: keep them sorted
+    print(divisors)
 
 divisor(36)
+# Output: [1, 2, 3, 4, 6, 9, 12, 18, 36]
